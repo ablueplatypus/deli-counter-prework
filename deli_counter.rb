ad78fc38f484bdbf0146629055f26db0ba487e1e
Matt Doering <mattdoering86@gmail.com>
2018-11-06 21:04:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,2 +1,8 @@
-
+def line(current_line)
+  if current_line.length == 0
+    puts "The line is currently empty."
+  else
+    message = "The line is currently:"
+    current_line.each_with_index do |name, number|
+    
 
