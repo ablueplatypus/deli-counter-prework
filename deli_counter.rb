1eb4886027614849e943507006a2502052c34ec9
Matt Doering <mattdoering86@gmail.com>
2018-11-06 21:38:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,9 @@ def line(current_line)
   if current_line.length == 0 
     puts "The line is currently empty."
   else
-    message = "The line is currently: "
+    message = "The line is currently:"
     current_line.each_with_index do |name, number|
-      message += "#{number + 1}. #{name} "
+      message += " #{number + 1}. #{name}"
     end
     puts message
   end 
